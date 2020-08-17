376d28d3e24aace4b276a86a4e5629c08641b8ed
zdinedine <tomasbizek@outlook.com>
2020-08-17 19:27:48 +0200

    initial version

---
+++ Login/login/accounts/urls.py
@@ -0,0 +1,9 @@
+from  django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.indexView, name="home"),
+    path('dashboard/', views.dashboardView,name="dashboard"),
+    path('login/',),
+    path('register/',),
+    path('logout/',),
+]
