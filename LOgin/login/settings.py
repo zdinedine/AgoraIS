7b6b15011ac5f7ac8afa0024a4aa979732693dea
zdinedine <tomasbizek@outlook.com>
2020-08-23 16:03:38 +0200

    Login done

--- LOgin/login/settings.py
+++ LOgin/login/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/accounts/dashboard/'
